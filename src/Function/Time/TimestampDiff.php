31e088edd4ce1cb8a0bf740b5a264148db096ba6
Krisi Gjika <Krisi.Gjika@fti.edu.al>
2025-11-20 16:58:09 +0100

    feat(timestampdiff): simplified TimestampDiff. fix typos in testing.

--- src/Function/Time/TimestampDiff.php
+++ src/Function/Time/TimestampDiff.php
@@ -9,11 +9,6 @@ use Illuminate\Database\Grammar;
 use Tpetry\QueryExpressions\Concerns\IdentifiesDriver;
 use Tpetry\QueryExpressions\Concerns\StringizeExpression;
 use Tpetry\QueryExpressions\Enums\TimeUnits;
-use Tpetry\QueryExpressions\Operator\Arithmetic\Add;
-use Tpetry\QueryExpressions\Operator\Arithmetic\Divide;
-use Tpetry\QueryExpressions\Operator\Arithmetic\Multiply;
-use Tpetry\QueryExpressions\Operator\Arithmetic\Subtract;
-use Tpetry\QueryExpressions\Value\Number;
 
 class TimestampDiff implements Expression
 {
@@ -34,79 +29,42 @@ class TimestampDiff implements Expression
         $to = $this->stringize($grammar, $this->to ?? new Now);
 
         return match ($this->identify($grammar)) {
-            'mariadb', 'mysql' => "(timestampdiff({$this->unit->value}, {$from}, {$to}))",
+            'mariadb', 'mysql' => "(timestampdiff({$this->unit->toMysql()}, {$from}, {$to}))",
             'sqlsrv' => "datediff({$this->unit->toSqlServer()}, {$to}, {$from})",
-            'pgsql' => $this->postgress($grammar, $from, $to),
-            'sqlite' => $this->sqlite($grammar, $from, $to),
+            'pgsql' => $this->postgress($from, $to),
+            'sqlite' => $this->sqlite($from, $to),
         };
     }
 
-    protected function postgress(Grammar $grammar, float|int|string $from, float|int|string $to): float|int|string
+    protected function postgress(float|int|string $from, float|int|string $to): float|int|string
     {
         return match ($this->unit) {
-            TimeUnits::SECOND => "extraxt(epoch from ({$from} - {$to})",
+            TimeUnits::SECOND => "extract(epoch from ({$from} - {$to})",
             TimeUnits::MINUTE,
             TimeUnits::HOUR,
             TimeUnits::DAY,
-            TimeUnits::WEEK => $this->postgressBase($from, $to)->getValue($grammar),
-            TimeUnits::MONTH => $this->postgressMonth($from, $to)->getValue($grammar),
+            TimeUnits::WEEK => "extract(epoch from ({$from} - {$to}) / {$this->unit->toSeconds()}",
+            TimeUnits::MONTH => "(extract(YEAR from age({$to}, {$from})) * 12) + extract(MONTH from age({$to}, {$from}))",
             TimeUnits::YEAR => "extract(YEAR from age({$to}, {$from}))",
         };
     }
 
-    protected function postgressBase(float|int|string $from, float|int|string $to): Expression
-    {
-        return new Divide(
-            "extraxt(epoch from ({$from} - {$to})",
-            new Number($this->unit->toSeconds())
-        );
-    }
-
-    protected function postgressMonth(float|int|string $from, float|int|string $to): Expression
-    {
-        return new Add(
-            new Multiply(
-                "extract(YEAR from age({$to}, {$from}))",
-                new Number(12) // months in a year
-            ),
-            "extract(MONTH from age({$to}, {$from}))"
-        );
-    }
-
-    protected function sqlite(Grammar $grammar, float|int|string $from, float|int|string $to): float|int|string
+    protected function sqlite(float|int|string $from, float|int|string $to): float|int|string
     {
         return match ($this->unit) {
-            TimeUnits::SECOND => "extraxt(epoch from ({$from} - {$to})",
+            TimeUnits::SECOND => "extract(epoch from ({$from} - {$to})",
             TimeUnits::MINUTE,
             TimeUnits::HOUR,
             TimeUnits::DAY,
-            TimeUnits::WEEK => $this->sqliteBase($from, $to)->getValue($grammar),
-            TimeUnits::MONTH => $this->sqliteMonth($from, $to)->getValue($grammar),
-            TimeUnits::YEAR => $this->sqliteYear($from, $to)->getValue($grammar),
+            TimeUnits::WEEK => "extract(epoch from ({$from} - {$to}) / {$this->unit->toSeconds()}",
+            TimeUnits::MONTH => with(
+                value: ['from' => $from, 'to' => $to],
+                callback: function ($value) {
+                    $yearsDiff = "strftime('%Y', {$value['to']}) - strftime('%Y', {$value['from']})";
+                    $monthsDiff = "strftime('%m', {$value['to']}) - strftime('%m', {$value['from']})";
+                    return "({$yearsDiff} * 12) + {$monthsDiff}";
+                }),
+            TimeUnits::YEAR => "strftime('%Y', {$to}) - strftime('%Y', {$from})",
         };
-    }
-
-    protected function sqliteBase(float|int|string $from, float|int|string $to): Expression
-    {
-        return new Divide(
-            "extraxt(epoch from ({$from} - {$to})",
-            new Number($this->unit->toSeconds())
-        );
-    }
-
-    protected function sqliteMonth(float|int|string $from, float|int|string $to): Expression
-    {
-        return new Add(
-            new Multiply(
-                new Subtract("strftime('%Y', {$to})", "strftime('%Y', {$from})"),
-                new Number(12) // months in a year
-            ),
-            new Subtract("strftime('%m', {$to})", "strftime('%m', {$from})"),
-        );
-    }
-
-    protected function sqliteYear(float|int|string $from, float|int|string $to): Expression
-    {
-        return new Subtract("strftime('%Y', {$to})", "strftime('%Y', {$from})");
     }
 }
--- tests/Function/Time/TimestampDiffTest.php
+++ tests/Function/Time/TimestampDiffTest.php
@@ -9,63 +9,63 @@ use Tpetry\QueryExpressions\Function\Time\TimestampDiff;
 it('can diff the time of two columns in seconds')
     ->expect(new TimestampDiff('val_1', TimeUnits::SECOND, 'val_2'))
     ->toBeExecutable(function (Blueprint $table) {
-        $table->timestamp('val_1');
-        $table->timestamp('val_2');
+        $table->timestamp('val_1')->useCurrent();
+        $table->timestamp('val_2')->useCurrent();
     })
     ->toBeMysql('(timestampdiff(SECOND, `val_1`, `val_2`))')
-    ->toBePgsql('extraxt(epoch from ("val_1" - "val_2")')
-    ->toBeSqlite('extraxt(epoch from ([val_1] - [val_2])')
+    ->toBePgsql('extract(epoch from ("val_1" - "val_2")')
+    ->toBeSqlite('extract(epoch from ([val_1] - [val_2])')
     ->toBeSqlsrv('(datediff(Second, "val_1", "val_2")');
 
 it('can diff the time of two columns in minutes')
     ->expect(new TimestampDiff('val_1', TimeUnits::MINUTE, 'val_2'))
     ->toBeExecutable(function (Blueprint $table) {
-        $table->timestamp('val_1');
-        $table->timestamp('val_2');
+        $table->timestamp('val_1')->useCurrent();
+        $table->timestamp('val_2')->useCurrent();
     })
     ->toBeMysql('(timestampdiff(MINUTE, `val_1`, `val_2`))')
-    ->toBePgsql('extraxt(epoch from ("val_1" - "val_2") / 60')
-    ->toBeSqlite('extraxt(epoch from ([val_1] - [val_2]) / 60')
-    ->toBeSqlsrv('(datediff(Minute, "vate, "val_1", "val_2")');
+    ->toBePgsql('extract(epoch from ("val_1" - "val_2") / 60')
+    ->toBeSqlite('extract(epoch from ([val_1] - [val_2]) / 60')
+    ->toBeSqlsrv('(datediff(Minute, "val_1", "val_2")');
 
 it('can diff the time of two columns in hours')
     ->expect(new TimestampDiff('val_1', TimeUnits::HOUR, 'val_2'))
     ->toBeExecutable(function (Blueprint $table) {
-        $table->timestamp('val_1');
-        $table->timestamp('val_2');
+        $table->timestamp('val_1')->useCurrent();
+        $table->timestamp('val_2')->useCurrent();
     })
     ->toBeMysql('(timestampdiff(HOUR, `val_1`, `val_2`))')
-    ->toBePgsql('extraxt(epoch from ("val_1" - "val_2") / 3600')
-    ->toBeSqlite('extraxt(epoch from ([val_1] - [val_2]) / 3600')
+    ->toBePgsql('extract(epoch from ("val_1" - "val_2") / 3600')
+    ->toBeSqlite('extract(epoch from ([val_1] - [val_2]) / 3600')
     ->toBeSqlsrv('(datediff(hour, "val_1", "val_2")');
 
 it('can diff the time of two columns in days')
     ->expect(new TimestampDiff('val_1', TimeUnits::DAY, 'val_2'))
     ->toBeExecutable(function (Blueprint $table) {
-        $table->timestamp('val_1');
-        $table->timestamp('val_2');
+        $table->timestamp('val_1')->useCurrent();
+        $table->timestamp('val_2')->useCurrent();
     })
     ->toBeMysql('(timestampdiff(DAY, `val_1`, `val_2`))')
-    ->toBePgsql('extraxt(epoch from ("val_1" - "val_2") / 86400')
-    ->toBeSqlite('extraxt(epoch from ([val_1] - [val_2]) / 86400')
+    ->toBePgsql('extract(epoch from ("val_1" - "val_2") / 86400')
+    ->toBeSqlite('extract(epoch from ([val_1] - [val_2]) / 86400')
     ->toBeSqlsrv('(datediff(Day, "val_1", "val_2")');
 
 it('can diff the time of two columns in weeks')
     ->expect(new TimestampDiff('val_1', TimeUnits::WEEK, 'val_2'))
     ->toBeExecutable(function (Blueprint $table) {
-        $table->timestamp('val_1');
-        $table->timestamp('val_2');
+        $table->timestamp('val_1')->useCurrent();
+        $table->timestamp('val_2')->useCurrent();
     })
     ->toBeMysql('(timestampdiff(WEEK, `val_1`, `val_2`))')
-    ->toBePgsql('extraxt(epoch from ("val_1" - "val_2") / 604800')
-    ->toBeSqlite('extraxt(epoch from ([val_1] - [val_2]) / 604800')
+    ->toBePgsql('extract(epoch from ("val_1" - "val_2") / 604800')
+    ->toBeSqlite('extract(epoch from ([val_1] - [val_2]) / 604800')
     ->toBeSqlsrv('(datediff(Week, "val_1", "val_2")');
 
 it('can diff the time of two columns in months')
     ->expect(new TimestampDiff('val_1', TimeUnits::MONTH, 'val_2'))
     ->toBeExecutable(function (Blueprint $table) {
-        $table->timestamp('val_1');
-        $table->timestamp('val_2');
+        $table->timestamp('val_1')->useCurrent();
+        $table->timestamp('val_2')->useCurrent();
     })
     ->toBeMysql('(timestampdiff(MONTH, `val_1`, `val_2`))')
     ->toBePgsql('(extract(YEAR from age("val_1", "val_2")) * 12) + extract(MONTH from age("val_1", "val_2"))')
@@ -75,10 +75,10 @@ it('can diff the time of two columns in months')
 it('can diff the time of two columns in years')
     ->expect(new TimestampDiff('val_1', TimeUnits::YEAR, 'val_2'))
     ->toBeExecutable(function (Blueprint $table) {
-        $table->timestamp('val_1');
-        $table->timestamp('val_2');
+        $table->timestamp('val_1')->useCurrent();
+        $table->timestamp('val_2')->useCurrent();
     })
     ->toBeMysql('(timestampdiff(YEAR, `val_1`, `val_2`))')
-    ->toBePgsql('(extract(YEAR from age("val_2", "val_2")))')
+    ->toBePgsql('extract(YEAR from age("val_2", "val_2"))')
     ->toBeSqlite('strftime(\'%Y\', [val_2]) - strftime(\'%Y\', [val_2])')
     ->toBeSqlsrv('(datediff(Year, "val_1", "val_2")');
